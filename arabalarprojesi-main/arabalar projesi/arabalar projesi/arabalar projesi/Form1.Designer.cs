23ce9be39b1b2fcc3e013d5351c7acf608598142
bilalozcelik <37147381+bilalozcelik@users.noreply.github.com>
2022-09-16 10:13:51 +0300

    Add files via upload

---
+++ arabalarprojesi-main/arabalar projesi/arabalar projesi/arabalar projesi/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace arabalar_projesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridArabalar = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMarka = new System.Windows.Forms.TextBox();
+            this.txtModel = new System.Windows.Forms.TextBox();
+            this.txtYil = new System.Windows.Forms.TextBox();
+            this.txtRenk = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtAra = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridArabalar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridArabalar
+            // 
+            this.dataGridArabalar.AllowUserToAddRows = false;
+            this.dataGridArabalar.AllowUserToDeleteRows = false;
+            this.dataGridArabalar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridArabalar.Location = new System.Drawing.Point(12, 201);
+            this.dataGridArabalar.Name = "dataGridArabalar";
+            this.dataGridArabalar.ReadOnly = true;
+            this.dataGridArabalar.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridArabalar.Size = new System.Drawing.Size(556, 150);
+            this.dataGridArabalar.TabIndex = 0;
+            this.dataGridArabalar.TabStop = false;
+            this.dataGridArabalar.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridArabalar_CellClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Marka";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Model";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Yıl";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Renk";
+            // 
+            // txtMarka
+            // 
+            this.txtMarka.Location = new System.Drawing.Point(137, 31);
+            this.txtMarka.Name = "txtMarka";
+            this.txtMarka.Size = new System.Drawing.Size(133, 20);
+            this.txtMarka.TabIndex = 1;
+            // 
+            // txtModel
+            // 
+            this.txtModel.Location = new System.Drawing.Point(137, 66);
+            this.txtModel.Name = "txtModel";
+            this.txtModel.Size = new System.Drawing.Size(133, 20);
+            this.txtModel.TabIndex = 2;
+            // 
+            // txtYil
+            // 
+            this.txtYil.Location = new System.Drawing.Point(137, 104);
+            this.txtYil.Name = "txtYil";
+            this.txtYil.Size = new System.Drawing.Size(133, 20);
+            this.txtYil.TabIndex = 3;
+            // 
+            // txtRenk
+            // 
+            this.txtRenk.Location = new System.Drawing.Point(137, 136);
+            this.txtRenk.Name = "txtRenk";
+            this.txtRenk.Size = new System.Drawing.Size(133, 20);
+            this.txtRenk.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(365, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Ara";
+            // 
+            // txtAra
+            // 
+            this.txtAra.Location = new System.Drawing.Point(394, 34);
+            this.txtAra.Name = "txtAra";
+            this.txtAra.Size = new System.Drawing.Size(133, 20);
+            this.txtAra.TabIndex = 5;
+            this.txtAra.TextChanged += new System.EventHandler(this.txtAra_TextChanged);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(317, 113);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(71, 43);
+            this.btnEkle.TabIndex = 4;
+            this.btnEkle.TabStop = false;
+            this.btnEkle.Text = "EKLE";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(394, 113);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(71, 43);
+            this.btnSil.TabIndex = 4;
+            this.btnSil.TabStop = false;
+            this.btnSil.Text = "SİL";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(471, 113);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(78, 43);
+            this.btnGuncelle.TabIndex = 4;
+            this.btnGuncelle.TabStop = false;
+            this.btnGuncelle.Text = "GÜNCELLE";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 382);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtAra);
+            this.Controls.Add(this.txtRenk);
+            this.Controls.Add(this.txtYil);
+            this.Controls.Add(this.txtModel);
+            this.Controls.Add(this.txtMarka);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridArabalar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridArabalar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridArabalar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMarka;
+        private System.Windows.Forms.TextBox txtModel;
+        private System.Windows.Forms.TextBox txtYil;
+        private System.Windows.Forms.TextBox txtRenk;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtAra;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGuncelle;
+    }
+}
+
---
+++ arabalarprojesi-main/arabalar projesi/arabalar projesi/arabalar projesi/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace arabalar_projesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        MySqlConnection baglanti = new MySqlConnection("Server=localhost;Database=vttasitlar;Uid=root;Pwd=bist.2022");
+        MySqlCommand komut;
+        string sorguCumlesi;
+    private void goster()
+        {
+            sorguCumlesi = "select * from arabalar";
+            MySqlDataAdapter adapter = new MySqlDataAdapter(sorguCumlesi, baglanti);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridArabalar.DataSource = dt;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            goster();
+        }
+
+        private void dataGridArabalar_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtMarka.Text = dataGridArabalar.CurrentRow.Cells["marka"].Value.ToString();
+            txtModel.Text = dataGridArabalar.CurrentRow.Cells["model"].Value.ToString();
+            txtYil.Text = dataGridArabalar.CurrentRow.Cells["yil"].Value.ToString();
+            txtRenk.Text = dataGridArabalar.CurrentRow.Cells["renk"].Value.ToString();
+        }
+        private void temizle()
+        {
+            txtMarka.Clear();
+            txtModel.Clear();
+            txtYil.Clear();
+            txtRenk.Text = "";
+
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            if(baglanti.State!=ConnectionState.Open)
+            { baglanti.Open(); }
+            sorguCumlesi = "insert into arabalar(marka,model,yil,renk) values(@marka,@model,@yil,@renk)";
+            komut = new MySqlCommand(sorguCumlesi, baglanti);
+            komut.Parameters.AddWithValue("@marka", txtMarka.Text);
+            komut.Parameters.AddWithValue("@model", txtModel.Text);
+            komut.Parameters.AddWithValue("@yil", txtYil.Text);
+            komut.Parameters.AddWithValue("@renk", txtRenk.Text);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            temizle();
+            goster();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            if (baglanti.State != ConnectionState.Open)
+            { baglanti.Open(); }
+            sorguCumlesi = "delete from arabalar where id=@id";
+            komut = new MySqlCommand(sorguCumlesi, baglanti);
+            komut.Parameters.AddWithValue("@id", dataGridArabalar.CurrentRow.Cells["id"].Value.ToString());
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            temizle();
+            goster();
+         }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            if (baglanti.State != ConnectionState.Open)
+            { baglanti.Open(); }
+            sorguCumlesi = "update arabalar set marka=@marka,model=@model,yil=@yil,renk=@renk where id=@id";
+            komut = new MySqlCommand(sorguCumlesi, baglanti);
+            komut.Parameters.AddWithValue("@id", dataGridArabalar.CurrentRow.Cells["id"].Value.ToString());
+            komut.Parameters.AddWithValue("@marka", txtMarka.Text);
+            komut.Parameters.AddWithValue("@model", txtModel.Text);
+            komut.Parameters.AddWithValue("@yil", txtYil.Text);
+            komut.Parameters.AddWithValue("@renk", txtRenk.Text);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            temizle();
+            goster();
+        }
+
+        private void txtAra_TextChanged(object sender, EventArgs e)
+        {
+            arama(txtAra.Text);
+        }
+        private void arama(string aramaKelimesi)
+        {
+            if (baglanti.State != ConnectionState.Open)
+            { baglanti.Open(); }
+            komut = new MySqlCommand();
+            komut.Connection = baglanti;
+            komut.CommandText = "select * from arabalar where marka like '" + aramaKelimesi + "%'";
+            MySqlDataAdapter dataadapter = new MySqlDataAdapter(komut);
+            DataTable dt = new DataTable();
+            dataadapter.Fill(dt);
+            baglanti.Close();
+            dataGridArabalar.DataSource = dt;
+        }
+    }
+}
